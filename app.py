5f1833c355470c148bf23d70387d5336334a5327
Louie Kim <hyeok0724.kim@gmail.com>
2020-10-13 15:52:53 +0900

    history calc insert

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ if __name__ == "__main__":
     #if platform.system() == "Linux":
     #    setproctitle.setproctitle('ninewatt_app')
 
-    app.run(host="127.0.0.1", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
